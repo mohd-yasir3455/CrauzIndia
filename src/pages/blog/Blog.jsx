99edfa0397eeb39cb6651d4c0c5f8cd30c324803
User1 <user1@mail.com>
2025-07-22 11:47:48 +0530

    Second commit

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Navbar from "../../components/navbar/Navbar"
+import Footer from "../../components/footer/Footer"
+import styles from "../blog/Blog.module.css"
 
 const Blog = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <h1>Blog</h1>
+      <Footer></Footer>
     </div>
   )
 }
